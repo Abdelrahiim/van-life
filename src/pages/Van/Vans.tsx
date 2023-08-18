7edeba23b0b49017b06ddd12590b9b3a2dfb1694
Abdelrahiim <eng_abdelrahiim@yahoo.com>
2023-08-19 01:35:35 +0300

    done Challenges

--- src/pages/Van/Vans.tsx
+++ src/pages/Van/Vans.tsx
@@ -20,7 +20,6 @@ export default function Vans() {
 	const [searchParams, setSearchParams] = useSearchParams();
 
 	const typeFilter = searchParams.get("type");
-	
 
 	const fetchDate = async () => {
 		try {
@@ -56,18 +55,27 @@ export default function Vans() {
 		<div className="van-list-container">
 			<h1>Explore Our Van Options</h1>
 			<div className="van-list-filter-buttons">
-				<Link className="van-type simple" to="?type=simple">
+				<Link className={`van-type simple ${typeFilter ==='simple' && "selected"}`} to="?type=simple">
 					Simple
 				</Link>
-				<button className="van-type rugged" onClick={()=> setSearchParams("type=rugged")}>
+				<button
+					className={`van-type rugged  ${typeFilter ==='simple' && "rugged"}`}
+					onClick={() => setSearchParams("type=rugged")}
+				>
 					Rugged
 				</button>
-				<button className="van-type luxury" onClick={()=> setSearchParams({type:"luxury"})}>
+
+				<button
+					className="van-type luxury"
+					onClick={() => setSearchParams({ type: "luxury" })}
+				>
 					Luxury
 				</button>
-				<Link className="van-type clear-filters" to=".">
-					Clear Filters
-				</Link>
+				{typeFilter && (
+					<Link className="van-type clear-filters" to=".">
+						Clear Filters
+					</Link>
+				)}
 			</div>
 			<div className="van-list">
 				{vans.length ? vanELements : <h2>Loading...</h2>}
